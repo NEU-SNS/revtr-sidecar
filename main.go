a93bce5d30bf2fc8087416cd726291eddf12727f
Roberto D'Auria <roberto@measurementlab.net>
2023-10-02 17:42:05 +0200

    Add constructor for revtrMeasurement

--- main.go
+++ main.go
@@ -211,9 +211,55 @@ func (h *handler) ProcessOpenEvents(ctx context.Context, revtrAPIKey string, rev
 	revtrSiteToIP[revtrTestSite] = revtrTestSrc
 	// Skeleton for a revtr measurement coming from M-Lab
 
-	revtrMeasurement := revtrpb.RevtrMeasurement{
-		Src: "", // Filled later
-		Dst: "", // Filled later
+	t := time.NewTicker(15 * time.Second)
+
+	revtrsToSend := []*revtrpb.RevtrMeasurement{}
+
+	for {
+		select {
+		case e := <-h.events:
+			log.Println("processing", e)
+			// Call the gRPC API of Reverse Traceroute
+			// Match the sources with the mapping of the revtr sites / IP addresses
+			// Check if we have a source in the same site as the NDT
+			if site, ok := h.mlabIPtoSite[e.id.SrcIP]; ok {
+				if revtrSrc, ok := revtrSiteToIP[site]; ok {
+					// Sample the revtrs
+					revtrMeasurementToSend := newRevtrMeasurement(revtrSrc, e.id.DstIP, e.uuid)
+
+					logger.Debugf("Adding reverse traceroute with source %s and destination %s to send", revtrMeasurementToSend.Src, revtrMeasurementToSend.Dst)
+					revtrsToSend = append(revtrsToSend, &revtrMeasurementToSend)
+				} else {
+					log.Infof("Site %s IP %s is not a revtr site", site, e.id.SrcIP)
+				}
+
+			} else {
+				log.Infof("No NDT site matching for IP %s", e.id.SrcIP)
+			}
+
+		case <-t.C:
+			if len(revtrsToSend) > 0 {
+				// Flush what we can flush
+				log.Infof("Collected batch of %d revtrs to send", len(revtrsToSend))
+				revtrs := make([]*revtrpb.RevtrMeasurement, len(revtrsToSend))
+				copy(revtrs, revtrsToSend)
+				go callRevtr(&client, revtrs, revtrAPIKey, revtrSampling)
+				revtrsToSend = nil
+			}
+		case <-ctx.Done():
+			log.Println("shutdown")
+			return
+		}
+	}
+}
+
+// newRevtrMeasurement creates a new revtr measurement with the given src, dst and uuid.
+func newRevtrMeasurement(src, dst, uuid string) revtrpb.RevtrMeasurement {
+	return revtrpb.RevtrMeasurement{
+		Src:  src,
+		Dst:  dst,
+		Uuid: uuid,
+
 		// Staleness is for the staleness of the atlas
 		RrVpSelectionAlgorithm: "ingress_cover",
 		UseTimestamp:           false,
@@ -237,51 +283,6 @@ func (h *handler) ProcessOpenEvents(ctx context.Context, revtrAPIKey string, rev
 		Label:                  "ndt_revtr_sidecar",
 		IsRunForwardTraceroute: false,
 		IsRunRttPings:          true,
-	}
-
-	t := time.NewTicker(15 * time.Second)
-
-	revtrsToSend := []*revtrpb.RevtrMeasurement{}
-
-	for {
-		select {
-		case e := <-h.events:
-			log.Println("processing", e)
-			// Call the gRPC API of Reverse Traceroute
-			revtrMeasurementToSend := new(revtrpb.RevtrMeasurement)
-			*revtrMeasurementToSend = revtrMeasurement
-			// Match the sources with the mapping of the revtr sites / IP addresses
-			// Check if we have a source in the same site as the NDT
-			if site, ok := h.mlabIPtoSite[e.id.SrcIP]; ok {
-				if revtrSrc, ok := revtrSiteToIP[site]; ok {
-					// Sample the revtrs
-
-					revtrMeasurementToSend.Src = revtrSrc
-					revtrMeasurementToSend.Dst = e.id.DstIP
-					revtrMeasurementToSend.Uuid = e.uuid
-					logger.Debugf("Adding reverse traceroute with source %s and destination %s to send", revtrMeasurementToSend.Src, revtrMeasurementToSend.Dst)
-					revtrsToSend = append(revtrsToSend, revtrMeasurementToSend)
-				} else {
-					log.Infof("Site %s IP %s is not a revtr site", site, e.id.SrcIP)
-				}
-
-			} else {
-				log.Infof("No NDT site matching for IP %s", e.id.SrcIP)
-			}
-
-		case <-t.C:
-			if len(revtrsToSend) > 0 {
-				// Flush what we can flush
-				log.Infof("Collected batch of %d revtrs to send", len(revtrsToSend))
-				revtrs := make([]*revtrpb.RevtrMeasurement, len(revtrsToSend))
-				copy(revtrs, revtrsToSend)
-				go callRevtr(&client, revtrs, revtrAPIKey, revtrSampling)
-				revtrsToSend = nil
-			}
-		case <-ctx.Done():
-			log.Println("shutdown")
-			return
-		}
 	}
 }
 
